588de502177c5028b427b71832fd85a45210c3de
Yong Pin Bong <yong.pin.bong@biotechfarms.net>
2021-03-31 16:34:34 +0800

    First commit

---
+++ src/data-access/sequelize/config/config.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+module.exports = {
+"development": {
+"username": process.env.DB_USER,
+"password": process.env.DB_PASS,
+"database": process.env.DB_NAME,
+"host": process.env.DB_HOST,
+"dialect": "postgres" ,
+"logging": false
+},
+"test": {
+"username": process.env.DB_USER,
+"password": process.env.DB_PASS,
+"database": process.env.DB_NAME,
+"host": process.env.DB_HOST,
+"dialect": "postgres" ,
+"logging": false
+},
+"production": {
+"username": "root",
+"password": null,
+"database": "database_production",
+"host": "127.0.0.1",
+"dialect": "mysql"
+}
+}
